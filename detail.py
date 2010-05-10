c5bfe7048e4dda4e15e119b8df2c71412facffa2
Andrew Cohen <andrew@herbndove.com>
2010-05-10 11:45:24 -0500

    Memcache the city list
    Save cpu

--- detail.py
+++ detail.py
@@ -1,17 +1,28 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from google.appengine.api import memcache
 
 import model
 import cityutils
 
+    
+
 class CityDetail(webapp.RequestHandler):
-    def get(self):
-            pparts = self.request.path_info.split('/')
-            city = pparts[2]
+    def getCityList(self):
+        cityList = memcache.get('cityList')
+        if cityList is None:
             query = model.City.all()
             cityList = []
             for c in query:
                 cityList.append(c.name)
+            if not memcache.add('cityList', cityList, cityutils.ONEDAY):
+                logging.error('memcache set failed')
+        return cityList
+    def get(self):
+            pparts = self.request.path_info.split('/')
+            city = pparts[2]
+            query = model.City.all()
+            for c in query:
                 if c.name == city:
                     theCity = c
                     groups = {'contact_info':[], 
@@ -28,5 +39,5 @@ class CityDetail(webapp.RequestHandler):
 
 
             self.response.out.write(template.render('templates/city.html', 
-                   {'name':theCity.name, 'cityList':cityList, 'groups':groups}))
+                   {'name':theCity.name, 'cityList':self.getCityList(), 'groups':groups}))
 
